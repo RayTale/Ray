6faaa1a603a9d74ce06276c713e67512cd09b0c4
wuming <553782463@qq.com>
2018-03-19 20:42:32 +0800

    解决跟postgresql的驱动兼容bug

--- Ray.Postgresql/StateStorage.cs
+++ Ray.Postgresql/StateStorage.cs
@@ -13,32 +13,27 @@ namespace Ray.Postgresql
         string deleteSql, getByIdSql, insertSql, updateSql;
         public StateStorage(SqlTable table)
         {
-            this.tableInfo = table;
+            tableInfo = table;
             deleteSql = $"DELETE FROM {tableInfo.SnapshotTable} where stateid=@StateId";
             getByIdSql = $"select data FROM {tableInfo.SnapshotTable} where stateid=@StateId";
             insertSql = $"INSERT into {tableInfo.SnapshotTable}(stateid,data)VALUES(@StateId,@Data)";
             updateSql = $"update {tableInfo.SnapshotTable} set data=Data where stateid=@StateId";
         }
-        public Task DeleteAsync(K id)
+        public async Task DeleteAsync(K id)
         {
-            return RayTask.Execute(async () =>
+            using (var conn = tableInfo.CreateConnection())
             {
-                using (var conn = tableInfo.CreateConnection())
-                {
-                    await conn.ExecuteAsync(deleteSql, new { StateId = id });
-                }
-            });
+                await conn.ExecuteAsync(deleteSql, new { StateId = id });
+            }
         }
 
         public async Task<T> GetByIdAsync(K id)
         {
-            byte[] state = await RayTask.Execute(async () =>
+            byte[] state;
+            using (var conn = tableInfo.CreateConnection())
             {
-                using (var conn = tableInfo.CreateConnection())
-                {
-                    return await conn.ExecuteScalarAsync<byte[]>(getByIdSql, new { StateId = id });
-                }
-            });
+                state = await conn.ExecuteScalarAsync<byte[]>(getByIdSql, new { StateId = id });
+            }
             if (state != null)
             {
                 using (var ms = new MemoryStream(state))
@@ -49,34 +44,28 @@ namespace Ray.Postgresql
             return null;
         }
 
-        public Task InsertAsync(T data)
+        public async Task InsertAsync(T data)
         {
-            return RayTask.Execute(async () =>
+            using (var ms = new PooledMemoryStream())
             {
-                using (var ms = new PooledMemoryStream())
+                Serializer.Serialize(ms, data);
+                using (var connection = tableInfo.CreateConnection())
                 {
-                    Serializer.Serialize(ms, data);
-                    using (var connection = tableInfo.CreateConnection())
-                    {
-                        await connection.ExecuteAsync(insertSql, new { data.StateId, Data = ms.ToArray() });
-                    }
+                    await connection.ExecuteAsync(insertSql, new { data.StateId, Data = ms.ToArray() });
                 }
-            });
+            }
         }
 
-        public Task UpdateAsync(T data)
+        public async Task UpdateAsync(T data)
         {
-            return RayTask.Execute(async () =>
+            using (var ms = new PooledMemoryStream())
             {
-                using (var ms = new PooledMemoryStream())
+                Serializer.Serialize(ms, data);
+                using (var connection = tableInfo.CreateConnection())
                 {
-                    Serializer.Serialize(ms, data);
-                    using (var connection = tableInfo.CreateConnection())
-                    {
-                        await connection.ExecuteAsync(updateSql, new { data.StateId, Data = ms.ToArray() });
-                    }
+                    await connection.ExecuteAsync(updateSql, new { data.StateId, Data = ms.ToArray() });
                 }
-            });
+            }
         }
     }
 }
