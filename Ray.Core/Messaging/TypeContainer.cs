6afd936bb15f4b38e670c596af928a66e4574fd3
zhouzehui <zhouzehui@extop.org>
2018-12-11 22:53:55 +0800

    修复获取Type失败

--- Ray.Core/Messaging/TypeContainer.cs
+++ Ray.Core/Messaging/TypeContainer.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq;
 
 namespace Ray.Core.Messaging
 {
     public static class TypeContainer
     {
-        private static readonly ConcurrentDictionary<string, Type> typeDict  = new ConcurrentDictionary<string, Type>();
+        private static readonly ConcurrentDictionary<string, Type> typeDict = new ConcurrentDictionary<string, Type>();
         public static bool TryGetValue(string typeName, out Type value)
         {
             value = typeDict.GetOrAdd(typeName, key =>
             {
+                var assemblyList = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic);
+                foreach (var assembly in assemblyList)
+                {
+                    var type = assembly.GetType(typeName, false);
+                    if (type != default)
+                    {
+                        return type;
+                    }
+                }
                 return Type.GetType(typeName, false);
             });
             return value == default;
