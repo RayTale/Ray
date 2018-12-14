bd0284ade302b26bd00c32404e8f5898c272157d
无名 <553782463@qq.com>
2018-12-14 18:42:03 +0800

    开始重写channel

--- Ray.Core/Internal/EventTaskWrapper.cs
+++ Ray.Core/Internal/EventTaskWrapper.cs
@@ -2,6 +2,16 @@
 
 namespace Ray.Core.Internal
 {
+    public class EventWrapper<K>
+    {
+        public EventWrapper(IEventBase<K> value, string uniqueId = null)
+        {
+            Value = value;
+            UniqueId = uniqueId;
+        }
+        public IEventBase<K> Value { get; set; }
+        public string UniqueId { get; set; }
+    }
     public class EventTaskWrapper<K>
     {
         public static EventTaskWrapper<K> Create(IEventBase<K> value, string uniqueId = null)
