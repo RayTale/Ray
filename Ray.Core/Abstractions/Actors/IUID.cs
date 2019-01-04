c4bc539f9ca9f4d563d1fd07158d092a55953959
无名 <553782463@qq.com>
2019-01-04 18:11:50 +0800

    优化分布式ID的性能

--- Ray.Core/Abstractions/Actors/IUID.cs
+++ Ray.Core/Abstractions/Actors/IUID.cs
@@ -1,10 +1,12 @@
 ﻿using System.Threading.Tasks;
 using Orleans;
+using Orleans.Concurrency;
 
 namespace Ray.Core.Abstractions.Actors
 {
     public interface IUID : IGrainWithStringKey
     {
+        [AlwaysInterleave]
         Task<long> NewLongID();
         Task<string> NewUtcID();
         Task<string> NewLocalID();
