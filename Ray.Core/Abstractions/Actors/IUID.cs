3a0705ff384ecb07fceb624818039987484c64f1
zhouzehui <zhouzehui@extop.org>
2019-01-05 15:08:49 +0800

    优化uid和lock功能

--- Ray.Core/Abstractions/Actors/IUID.cs
+++ Ray.Core/Abstractions/Actors/IUID.cs
@@ -6,9 +6,17 @@ namespace Ray.Core.Abstractions.Actors
 {
     public interface IUID : IGrainWithStringKey
     {
+        /// <summary>
+        /// 通过utc时间生成分布式唯一id
+        /// </summary>
+        /// <returns>19位数字字符，支持转换为Int64类型</returns>
         [AlwaysInterleave]
-        Task<long> NewLongID();
         Task<string> NewUtcID();
+        /// <summary>
+        /// 通过本地时间生成分布式唯一id
+        /// </summary>
+        /// <returns>19位数字字符，支持转换为Int64类型</returns>
+        [AlwaysInterleave]
         Task<string> NewLocalID();
     }
 }
