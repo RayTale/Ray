34718062d7c39c6923e0543b0f2016d6b94b83bb
zhouzehui <zhouzehui@extop.org>
2019-01-06 10:15:54 +0800

    拆解rabbitmq的consumer manager方法

---
+++ Ray.RabbitMQ/ConsumerRunnerSlice.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client.Events;
+
+namespace Ray.EventBus.RabbitMQ
+{
+    public class ConsumerRunnerSlice
+    {
+        public ModelWrapper Channel { get; set; }
+        public EventingBasicConsumer BasicConsumer { get; set; }
+        public ushort Qos { get; set; }
+        public bool NeedRestart { get; set; }
+        public bool IsUnAvailable => NeedRestart || !BasicConsumer.IsRunning || Channel.Model.IsClosed;
+        public void Close()
+        {
+            if (Channel != default && Channel.Model.IsOpen)
+            {
+                if (!NeedRestart)
+                    BasicConsumer.Model.Abort();
+                else
+                    BasicConsumer.Model.Close();
+                BasicConsumer.Model.Dispose();
+            }
+            Channel?.Dispose();
+        }
+    }
+}
