b26f2bd87a0188a2127627e53b342d836a071333
无名 <553782463@qq.com>
2019-02-21 16:17:06 +0800

    添加IStorageConfig文件

---
+++ Ray.Core/Storage/IStorageConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Ray.Core.Storage
+{
+    public interface IStorageConfig
+    {
+        bool Singleton { get; set; }
+        ValueTask Init();
+    }
+    public interface IStorageConfigParameter
+    {
+        bool Singleton { get; set; }
+    }
+}
