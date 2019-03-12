4fdfdffc77bf30d12dadaa14e1dfff286f68403d
feijie <feijie89@qq.com>
2019-03-12 10:38:25 +0800

    1

--- Example/Ray.Host/Program.cs
+++ Example/Ray.Host/Program.cs
@@ -58,7 +58,7 @@ namespace Ray.MongoHost
                     //注册postgresql为事件存储库
                     servicecollection.AddPostgreSQLStorage(config =>
                     {
-                        config.ConnectionDict.Add("core_event", "Server=127.0.0.1;Port=5432;Database=Ray;User Id=postgres;Password=extop;Pooling=true;MaxPoolSize=20;");
+                        config.ConnectionDict.Add("core_event", "Server=127.0.0.1;Port=5432;Database=ray;User Id=postgres;Password=123456;Pooling=true;MaxPoolSize=20;");
                     });
                     servicecollection.AddPostgreSQLTransactionStorage(options =>
                     {
@@ -79,8 +79,8 @@ namespace Ray.MongoHost
                     //servicecollection.MongoConfigure();
                     servicecollection.AddRabbitMQ(config =>
                     {
-                        config.UserName = "admin";
-                        config.Password = "admin";
+                        config.UserName = "guest";
+                        config.Password = "guest";
                         config.Hosts = new[] { "127.0.0.1:5672" };
                         config.MaxPoolSize = 100;
                         config.VirtualHost = "/";
