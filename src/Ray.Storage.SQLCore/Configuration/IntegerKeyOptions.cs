588cb124633b7e9da967857593b17980536978cf
uless <zhouzehui@extop.org>
2019-04-07 11:05:27 +0800

    分表间隔天数参数名错误修复

--- src/Ray.Storage.SQLCore/Configuration/IntegerKeyOptions.cs
+++ src/Ray.Storage.SQLCore/Configuration/IntegerKeyOptions.cs
@@ -4,11 +4,11 @@ namespace Ray.Storage.SQLCore.Configuration
 {
     public class IntegerKeyOptions : StorageOptions
     {
-        public IntegerKeyOptions(IServiceProvider serviceProvider, string connectionKey, string uniqueName, long subTableMinutesInterval = 30) : base(serviceProvider)
+        public IntegerKeyOptions(IServiceProvider serviceProvider, string connectionKey, string uniqueName, long subTableDaysInterval = 30) : base(serviceProvider)
         {
             ConnectionKey = connectionKey;
             UniqueName = uniqueName;
-            SubTableMillionSecondsInterval = subTableMinutesInterval * 24 * 60 * 60 * 1000;
+            SubTableMillionSecondsInterval = subTableDaysInterval * 24 * 60 * 60 * 1000;
         }
     }
 }
