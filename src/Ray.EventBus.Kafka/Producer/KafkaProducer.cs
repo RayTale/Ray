dda37d9ec64b8ea7792614ff77815e8803e49ad7
阿凌 <510415008@qq.com>
2019-08-07 14:22:07 +0800

    Optimize Kafka performance

--- src/Ray.EventBus.Kafka/Producer/KafkaProducer.cs
+++ src/Ray.EventBus.Kafka/Producer/KafkaProducer.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 
 namespace Ray.EventBus.Kafka
 {
+    /// <summary>
+    /// Message producer
+    /// </summary>
     public class KafkaProducer : IProducer
     {
         readonly KafkaEventBus publisher;
@@ -15,13 +18,15 @@ namespace Ray.EventBus.Kafka
             this.publisher = publisher;
             this.client = client;
         }
-        public async ValueTask Publish(byte[] bytes, string hashKey)
+        public ValueTask Publish(byte[] bytes, string hashKey)
         {
             var topic = publisher.GetRoute(hashKey);
             using (var producer = client.GetProducer())
             {
-                await producer.Handler.ProduceAsync(topic, new Message<string, byte[]> { Key = hashKey, Value = bytes });
+                producer.Handler.Produce(topic, new Message<string, byte[]> { Key = hashKey, Value = bytes });
             }
+            return new ValueTask(Task.CompletedTask);
+
         }
     }
 }
