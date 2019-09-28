7cd597c7c8e302100355ebf40432c262b667d721
uless <lessmoregood@outlook.com>
2019-09-28 18:20:25 +0800

    Delete expired demo.

--- examples/Transfer/Ray.IGrains/TransactionUnits/Inputs/TransferInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ray.IGrains.TransactionUnits.Inputs
-{
-    public class TransferInput
-    {
-        public long FromId { get; set; }
-        public long ToId { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
