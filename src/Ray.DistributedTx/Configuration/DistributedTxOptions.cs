cb06c4d6f203c2521f80f424e3c81c134e86bda7
uless <lessmoregood@outlook.com>
2019-10-02 13:57:33 +0800

    Fine tuning.

--- src/Ray.DistributedTx/Configuration/DistributedTxOptions.cs
+++ src/Ray.DistributedTx/Configuration/DistributedTxOptions.cs
@@ -3,8 +3,8 @@
     public class DistributedTxOptions
     {
         /// <summary>
-        /// 事务完成时候是否清理事务事件(事务事件只用来处理事务，不会影响状态)
-        /// true:减少事件量，但是会带来额外的性能开销
+        /// 事务完成时候是否保留事务事件(事务事件只用来处理事务，不会影响状态)
+        /// true:保留事务事件，提高吞吐，单位额外增加事件量
         /// 默认为(false)
         /// </summary>
         public bool RetainTxEvents { get; set; } = false;
