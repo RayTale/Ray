40159814e64404c8c49d12fe5e2fb515c16b5a10
tooth <slm1800@outlook.com>
2019-11-07 17:45:29 +0800

    修改序列化工具为newtonsoft

--- src/Ray.Core/Serialization/DefaultJsonSerializer.cs
+++ src/Ray.Core/Serialization/DefaultJsonSerializer.cs
@@ -1,48 +1,48 @@
 ﻿using System;
-using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Unicode;
+using Newtonsoft.Json;
 
 namespace Ray.Core.Serialization
 {
     public class DefaultJsonSerializer : ISerializer
     {
-        static readonly JsonSerializerOptions options = new JsonSerializerOptions() { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All) };
+        static readonly JsonSerializerSettings options = new JsonSerializerSettings() ;
         public T Deserialize<T>(string json) where T : class, new()
         {
-            return JsonSerializer.Deserialize<T>(json);
+            return JsonConvert.DeserializeObject<T>(json);
         }
 
         public object Deserialize(byte[] bytes, Type type)
         {
-            return JsonSerializer.Deserialize(bytes, type);
+            return JsonConvert.DeserializeObject(System.Text.UTF8Encoding.UTF8.GetString(bytes), type);
         }
         public string Serialize<T>(T data) where T : class, new()
         {
-            return JsonSerializer.Serialize(data, options);
+            return JsonConvert.SerializeObject(data, typeof(T), Formatting.None, options);
         }
         public string Serialize(object data, Type type)
         {
-            return JsonSerializer.Serialize(data, type, options);
+            return JsonConvert.SerializeObject(data, type, Formatting.None, options);
         }
         public byte[] SerializeToUtf8Bytes<T>(T data) where T : class, new()
         {
-            return JsonSerializer.SerializeToUtf8Bytes(data, data.GetType(), options);
+            var s = Serialize<T>(data);
+            return System.Text.UTF8Encoding.UTF8.GetBytes(s);
         }
 
         public T Deserialize<T>(byte[] bytes) where T : class, new()
         {
-            return JsonSerializer.Deserialize<T>(bytes);
+            return JsonConvert.DeserializeObject<T>(System.Text.UTF8Encoding.UTF8.GetString(bytes));
         }
 
         public byte[] SerializeToUtf8Bytes(object data, Type type)
         {
-            return JsonSerializer.SerializeToUtf8Bytes(data, type, options);
+            var s = Serialize(data,type);
+            return System.Text.UTF8Encoding.UTF8.GetBytes(s);
         }
 
         public object Deserialize(string json, Type type)
         {
-            return JsonSerializer.Deserialize(json, type);
+            return JsonConvert.DeserializeObject(json,type);
         }
     }
 }
